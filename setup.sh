a87d632343b2d7db01faa337c185a942e66dac4a
cosminflorea <floreailiecosmin@gmail.com>
2020-11-09 13:53:04 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "\
+[general]\n\
+email = \"floreailiecosmin@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
