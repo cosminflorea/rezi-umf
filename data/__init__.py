7bf1268832335406fc5c088b673363a88822d305
cosminflorea <floreailiecosmin@gmail.com>
2022-09-30 16:27:01 +0300

    Update __init__.py
    Adding 2021.pdf table

--- data/__init__.py
+++ data/__init__.py
@@ -4,5 +4,6 @@ data_path = ROOT_DIR = os.path.abspath(os.curdir)
 pdf_files = {"2017": os.path.join(data_path, "data", "2017.pdf"),
              "2018": os.path.join(data_path, "data", "2018.pdf"),
              "2019": os.path.join(data_path, "data", "2019.pdf"),
-             "2020": os.path.join(data_path, "data", "2020.pdf")
+             "2020": os.path.join(data_path, "data", "2020.pdf"),
+             "2021": os.path.join(data_path, "data", "2021.pdf")
              }
